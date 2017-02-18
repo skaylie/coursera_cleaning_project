4587251491f9e487a7ce2cc00428db2d0d515245
Scott Kaylie <scott.kaylie@gmail.com>
2017-02-18 12:48:05 -0800

    tweaks to run_analysis.R and start of documentation in README.md

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# coursera_cleaning_project
-Project repo for week 4 of getting and cleaning data course
+## Getting and Cleaning Data Course Project
+Project repo for week 4 of Getting andCcleaning Data course
+
+#Script
+The script for performing the process of getting and cleaning data is called run_analysis.R.  This script: 
+
+1. retrieves the source data from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip, combines the 
+2. adds columns for the subject and activity performed for each observation.  Possible activities are: WALKING,
+WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING
+3. adds field labels to the data
+4. selects only those columns that represent the mean or standard deviation (std) of their respective measurements
+5. replaces the index number of respective
--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,9 @@
 
 #get file, unzip it and load it
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "raw_data.zip")
+#download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "raw_data.zip")
 
-data_labels <- read.table(unz("raw_data.zip", "UCI HAR Dataset/features.txt"), header=F, quote="\"", sep=" ")
+library(dplyr)
+
 test_data <- read.table(unz("raw_data.zip", "UCI HAR Dataset/test/X_test.txt"), nrows=50, header=F, quote="\"", sep="")
 train_data <- read.table(unz("raw_data.zip", "UCI HAR Dataset/train/X_train.txt"), nrows=50, header=F, quote="\"", sep="")
 
@@ -13,20 +14,22 @@ test_activities <- read.table(unz("raw_data.zip", "UCI HAR Dataset/test/y_test.t
 train_activities <- read.table(unz("raw_data.zip", "UCI HAR Dataset/train/y_train.txt"), nrows=50, header=F, quote="\"", sep="")
 
 activity_labels <- read.table(unz("raw_data.zip", "UCI HAR Dataset/activity_labels.txt"), nrows=50, header=F, quote="\"", sep="")
+names(activity_labels) <- c("activity_num", "activity_name")
 
-#select and bind pertinent columns
-test_narrow <- select(test_data, 1:6)
-test_narrow <- cbind(test_subjects, test_narrow)
-test_narrow <- cbind(test_activities, test_narrow)
+#bind subject and activity columns to raw data for both test and train data sets
+test_data <- cbind(test_subjects, test_data)
+test_data <- cbind(test_activities, test_data)
 
-train_narrow <- select(train_data, 1:6)
-train_narrow <- cbind(test_subjects, test_narrow)
-train_narrow <- cbind(test_activities, test_narrow)
+train_data <- cbind(train_subjects, train_data)
+train_data <- cbind(train_activities, train_data)
 
-raw_data <- rbind(test_narrow, train_narrow)
-raw_data$observation <- 1:nrow(raw_data)
-names(raw_data) <- gsub(pattern="[()]", replacement="", x = data_labels[1:6,2])
+raw_data <- rbind(test_data, train_data)
 
-names(with_cols) <- c("subject", "activity", "acceleration_mean_x", "acceleration_mean_y", "acceleration_mean_z", "acceleration_std_dev_x", "acceleration_std_dev_y", "acceleration_std_dev_z")
-joined <- merge(with_all, activity_labels, by.x = "activity", by.y = "V1")
-molten <- melt(with_cols, id.vars=c("subject", "activity"))
+#get and add data labels
+data_labels <- read.table(unz("raw_data.zip", "UCI HAR Dataset/features.txt"), header=F, quote="\"", sep=" ")
+names(raw_data) <- c("activity_num", "subject", gsub(pattern="[()]", replacement="", x = data_labels[,2]))
+
+raw_data <- select(raw_data, c(activity, subject, grep("mean|std", names(raw_data)))) #select cols with mean and std in name
+
+joined <- merge(activity_labels, raw_data, by.x = "activity_num", by.y = "activity_num") #join activity labels to raw_data
+final_output <- joined %>% group_by(activity_name, subject) %>% summarize_each(funs(mean)) # output dataset with mean of each activity_name, subject for every variable
